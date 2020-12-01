39c1449779ad4288c045fe636801132420fb9212
Aditya Dev Tewari <32820043+AdityaDTewari@users.noreply.github.com>
2020-12-01 13:15:06 +0530

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,7 +23,7 @@ def run_game():
     aliens = Group()
 
     #creating the alien fleet
-    gf.create_fleet(ai_settings, screen,  aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     #a group to store bullets
     bullets = Group()
