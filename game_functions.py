f54dbff34b38d2343ae482f7fb7be4a8ea848838
Aditya Dev Tewari <32820043+AdityaDTewari@users.noreply.github.com>
2020-12-09 12:34:05 +0530

    Add files via upload

--- game_functions.py
+++ game_functions.py
@@ -25,7 +25,7 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
             ship.moving_left = False
 
-def check_events(ai_settings, screen, stats, play_button, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
     """for keyboard and mouse"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -38,12 +38,22 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
            check_keyup_events(event, ship)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(stats, play_button, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """start a new game when play is clicked"""
     if play_button.rect.collidepoint(mouse_x, mouse_y):
+        #reset statistics
+        stats.reset_stats()
         stats.game_active = True
+    
+    #empty the list of aliens and bullets
+    aliens.empty()
+    bullets.empty()
+
+    #create a new fleet and center the ship
+    create_fleet(ai_settings, screen, ship, aliens)
+    ship.center_ship()
 
 def update_screen(ai_settings, screen, stats, ship, bullets, aliens, play_button):
     """for redrawing the screen"""
